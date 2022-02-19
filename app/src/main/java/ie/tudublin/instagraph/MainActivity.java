359d58669490a81e54f5b0550012430b694ae3c6
Thomas Coll <thomascoll41@gmail.com>
2022-02-19 16:31:54 +0000

    Added functionality in MainActivity.java to toggle the visibility of the info window using the info button.
    Updated themes.xml:
    - Updated theme for information window to include more attributes
    - Added a theme for information button
    - Added a theme for the title
    - Added a theme for the logo
    
    Updated strings.xml:
    - Updated contents of the information string
    
    Updated constants.xml:
    - Added info text size and padding
    - Deleted title_and_info_gap
    - Added logo_and_start_gap
    - Added logo width and height
    - Added title_and_logo_gap
    - Added into button width and height
    - Added title_and_info_button_gap

--- app/src/main/java/ie/tudublin/instagraph/MainActivity.java
+++ app/src/main/java/ie/tudublin/instagraph/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button start;
-    Button info;
+    Button info_button;
+    TextView info_window;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,22 +19,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         start = findViewById(R.id.start);
-        info = findViewById(R.id.info);
+        info_button = findViewById(R.id.info_button);
+        info_window = findViewById(R.id.info_window);
 
         start.setOnClickListener(this);
-        info.setOnClickListener(this);
+        info_button.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch(view.getId()) {
             case(R.id.start):
+                // Make information window invisible
+                info_window.setVisibility(View.INVISIBLE);
+
                 Intent goToGetData = new Intent(MainActivity.this, GetDataActivity.class);
                 startActivity(goToGetData);
                 break;
 
-            case(R.id.info):
+            case(R.id.info_button):
                 // Display info window
+                if(info_window.getVisibility() == View.INVISIBLE) {
+                    info_window.setVisibility(View.VISIBLE);
+                }
+                else {
+                    info_window.setVisibility(View.INVISIBLE);
+                }
                 break;
         }
     }
