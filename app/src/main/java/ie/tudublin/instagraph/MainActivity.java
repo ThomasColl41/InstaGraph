a04eba17c823dfcde7d249db95cc760a0be48ad9
Thomas Coll <thomascoll41@gmail.com>
2022-03-01 12:00:59 +0000

    Added: - popup.xml that defines the layout for pop-up windows
    Updated MainActivity.java:
    - Removed Python initialisation comments
    - Added pop-up window functionality
    
    Updated:
    - themes.xml - altered the theme for the information window to suit a pop-up window
    - strings.xml - added a string for an error message and a pop-up placeholder string
    - constants.xml - changed info_text and info_padding to popup_text and popup_padding respectively
    
    Added an id to the main RelativeLayout in activity_main.xml to facilitate pop-up windows.
    Renamed instagraph_info_background.xml to instagraph_popup_background.xml.

--- app/src/main/java/ie/tudublin/instagraph/MainActivity.java
+++ app/src/main/java/ie/tudublin/instagraph/MainActivity.java
@@ -4,34 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-
-import com.chaquo.python.PyObject;
-import com.chaquo.python.Python;
-import com.chaquo.python.android.AndroidPlatform;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button start;
     Button info_button;
-    TextView info_window;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        // Initialise Python (using Chaquopy)
-//        if(!Python.isStarted()) {
-//            Python.start(new AndroidPlatform(this));
-//        }
-//
-//        Python py = Python.getInstance();
-
         start = findViewById(R.id.start);
         info_button = findViewById(R.id.info_button);
-        info_window = findViewById(R.id.info_window);
 
         start.setOnClickListener(this);
         info_button.setOnClickListener(this);
@@ -41,21 +34,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch(view.getId()) {
             case(R.id.start):
-                // Make information window invisible
-                info_window.setVisibility(View.INVISIBLE);
-
                 Intent goToGetData = new Intent(MainActivity.this, GetDataActivity.class);
                 startActivity(goToGetData);
                 break;
 
             case(R.id.info_button):
-                // Display info window
-                if(info_window.getVisibility() == View.INVISIBLE) {
-                    info_window.setVisibility(View.VISIBLE);
-                }
-                else {
-                    info_window.setVisibility(View.INVISIBLE);
-                }
+                // Display info pop-up
+                RelativeLayout parent = findViewById(R.id.main_layout);
+                LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+                View information = inflater.inflate(R.layout.popup,null);
+                int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                PopupWindow info_popup = new PopupWindow(information, width, height, true);
+                TextView textForPopup = information.findViewById(R.id.info_popup);
+                textForPopup.setText(R.string.instagraph_information);
+                info_popup.showAtLocation(parent, Gravity.CENTER, 0,0);
+                information.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View view, MotionEvent motionEvent) {
+                        info_popup.dismiss();
+                        return true;
+                    }
+                });
+
                 break;
         }
     }
