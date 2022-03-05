4a786fdfe417e0db305b375465633cc596b15067
Thomas Coll <thomascoll41@gmail.com>
2022-03-05 15:22:51 +0000

    Added Spinner functionality to SelectColumnsActivity.java.

--- app/src/main/java/ie/tudublin/instagraph/SelectColumnsActivity.java
+++ app/src/main/java/ie/tudublin/instagraph/SelectColumnsActivity.java
@@ -6,18 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SelectColumnsActivity extends AppCompatActivity implements View.OnClickListener {
     Button next;
     Button back;
 
+    Spinner modelSpinner;
+    Spinner col1Spinner;
+    Spinner col2Spinner;
+
     Python py;
     PyObject instaGraphPyObject;
+
+    private final List<String> models = new ArrayList<>();
+    private final List<String> columns = new ArrayList<>();
 
     String url;
 
@@ -51,9 +64,14 @@ public class SelectColumnsActivity extends AppCompatActivity implements View.OnC
 
         next = findViewById(R.id.next);
         back = findViewById(R.id.back);
+        modelSpinner = findViewById(R.id.model_spinner);
+        col1Spinner = findViewById(R.id.column_one);
+        col2Spinner = findViewById(R.id.column_two);
 
         next.setOnClickListener(this);
         back.setOnClickListener(this);
+
+        spinnerSetup(names);
     }
 
     @Override
@@ -61,6 +79,9 @@ public class SelectColumnsActivity extends AppCompatActivity implements View.OnC
         switch(view.getId()) {
             case(R.id.next):
                 Intent goToPredict = new Intent(SelectColumnsActivity.this, PredictActivity.class);
+                goToPredict.putExtra("model", modelSpinner.getSelectedItem().toString());
+                goToPredict.putExtra("col1", col1Spinner.getSelectedItem().toString());
+                goToPredict.putExtra("col2", col2Spinner.getSelectedItem().toString());
                 startActivity(goToPredict);
                 break;
 
@@ -68,5 +89,30 @@ public class SelectColumnsActivity extends AppCompatActivity implements View.OnC
                 finish();
                 break;
         }
+    }
+
+    public void spinnerSetup(String[] colNames) {
+        models.add("AR");
+        models.add("ARIMA");
+        models.add("SES");
+        models.add("HWES");
+        ArrayAdapter<String> modelArray = new ArrayAdapter<>(
+                this,
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                models);
+        modelArray.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        modelSpinner.setAdapter(modelArray);
+        modelArray.notifyDataSetChanged();
+
+        columns.addAll(Arrays.asList(colNames));
+
+        ArrayAdapter<String> columnArray = new ArrayAdapter<>(
+                this,
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                columns);
+        columnArray.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        col1Spinner.setAdapter(columnArray);
+        col2Spinner.setAdapter(columnArray);
+        columnArray.notifyDataSetChanged();
     }
 }
