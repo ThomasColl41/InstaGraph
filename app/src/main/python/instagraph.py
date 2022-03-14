4235aff9152fc80e28d5f0e01fd97c1f4f2ffc70
Thomas Coll <thomascoll41@gmail.com>
2022-03-14 22:48:26 +0000

    Updated build.gradle: - Reordered installs - Installed scikit-learn rather than sklearn (no difference)
    - Updated instagraph.py:
    - Moved the setting of x and y labels and title
    - Removed unused code, some of which referenced pmdarima which is not supported by Chaquopy

--- app/src/main/python/instagraph.py
+++ app/src/main/python/instagraph.py
@@ -6,7 +6,6 @@ import io
 import csv
 import urllib.request
 from os.path import dirname, join
-# import pmdarima as pm
 from statsmodels.tsa.ar_model import AutoReg, ar_select_order
 
 # Function to read in a dataset at a specified url
@@ -235,8 +234,8 @@ def horizontal_bar_chart_plot(url, xlabel='x-axis', ylabel='y-axis', title='Titl
         return str(te + ' has occurred, check column choices')
 
     # Set axis labels and title
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
+    ax.set_xlabel(ylabel)
+    ax.set_ylabel(xlabel)
     plt.title(title)
 
     # Display plot
@@ -248,214 +247,6 @@ def horizontal_bar_chart_plot(url, xlabel='x-axis', ylabel='y-axis', title='Titl
     buffer = io_buff.getvalue()
 
     return buffer
-
-    # Function to calculate model aicc
-def model_aicc(data, seasonal=False, periods=1):
-    model = None
-
-    # aicc is initially infinity (the most unsuitable)
-    aicc = np.inf
-
-    # Limit number of lags to check to 12 to reduce execution time
-    max_lag = 12
-
-    # Create model differently if there is seasonality
-    if seasonal:
-        # Determine appropriate lag structure
-        model_lags = ar_select_order(data, maxlag = max_lag, ic='aic', seasonal = True, period = periods).ar_lags
-
-        # Create model and calculate AICc
-        try:
-            model = AutoReg(data, lags = model_lags, seasonal = True, period = periods).fit()
-            aicc = model.aicc
-        except ZeroDivisionError as zde:
-            print(zde, 'has occurred in period ' + str(periods) + ' model')
-    else:
-        # Determine appropriate lag structure
-        model_lags = ar_select_order(data, maxlag = max_lag, ic='aic').ar_lags
-
-        # Create model and calculate AICc
-        try:
-            model = AutoReg(data, lags = model_lags).fit()
-            aicc = model.aicc
-        except ZeroDivisionError as zde:
-            print(zde, 'has occurred in period ' + str(periods) + ' model')
-
-    return aicc
-
-# Function to estimate seasonality
-def predict_seasonality(data):
-    # Determine the aicc of three models:
-    # 1. A simple model with no seasonality
-    # 2. A model with a seasonality of 7 periods
-    # 3. A model with a seasonality of 12 periods
-    order_aicc = model_aicc(data)
-    s7_order_aicc = model_aicc(data, True, 6)
-    s12_order_aicc = model_aicc(data, True, 11)
-
-    # Find the lowest aicc and thus, the most appropriate model
-    min_aicc = min([order_aicc, s7_order_aicc, s12_order_aicc])
-
-    # Return the period of the best model, rather than the model itself
-    if s7_order_aicc == min_aicc:
-        return 7
-    elif s12_order_aicc == min_aicc:
-        return 12
-    else:
-        return 1
-
-
-# Function to create a model and return the original data plus predictions
-def model_predict(data, model_type = 'AR'):
-    # Model will predict up to 10% of the data's length ahead
-    num_predictions = round(len(data) * 0.1)
-
-    # Minimum and maximum predictaions are 12 and 100 respectively
-    if num_predictions < 12:
-        num_predictions = 12
-    elif num_predictions > 100:
-        num_predictions = 100
-
-    # Create the appropriate model depending on user choice
-    if model_type == 'AR':
-        # Create model
-        full_data = ar_model_predict(data, num_predictions)
-
-
-    ### ARIMA, SES and HWES models not implemented ###
-    elif model_type == 'ARIMA':
-        # full_data = arima_model_predict(data, num_predictions)
-        print("ARIMA")
-    elif model_type == 'SES':
-        # full_data = ses_model_predict(data, num_predictions)
-        print('SES')
-    elif model_type == 'HWES':
-        # full_data = hwes_model_predict(data, num_predictions)
-        print('HWES')
-    # Default to AR model
-    else:
-        full_data = ar_model_predict(data, num_predictions)
-
-    return full_data, num_predictions
-
-# Using an autoregressive model, predict future values
-# Since data is assumed to be stationary at this point, there are no accommodations for seasonality
-def ar_model_predict(data, num_predictions=12):
-    # Limit number of lags to check to 12 to reduce execution time
-    max_lag = 12
-
-    # Determine appropriate lag structure
-    model_lags = ar_select_order(data, maxlag = max_lag, ic='aic').ar_lags
-    try:
-        # Try and create a model with the appropriate lag structure
-        model = AutoReg(data, lags = model_lags).fit()
-    except ZeroDivisionError as zde:
-        print(zde, 'has occurred in prediction model')
-
-        # Default to most basic AR model
-        model = AutoReg(data, lags = 1).fit()
-    finally:
-        # Predict data and append it to original dataset
-        predictions = model.forecast(num_predictions)
-        full_data = data[data.columns[0]].append(predictions)
-
-    return full_data
-
-
-
-# Function to difference data
-def difference(data, order=1, period=1):
-    if order > 2:
-        print('Order of differencing higher than 2 not implemented')
-        return data
-    else:
-        # Difference the data
-        differenced_data = data.diff(periods=period)
-
-        # Replace NaNs with the original data so the differenced data can be undifferenced
-        differenced_data.iloc[:period] = data.iloc[:period]
-
-        if order == 2:
-            # Difference again
-            differenced_data2 = differenced_data.diff(periods=period)
-
-            # Replace NaNs with the original data so the differenced data can be undifferenced
-            differenced_data2.iloc[:period] = differenced_data.iloc[:period]
-            return differenced_data2
-        else:
-            return differenced_data
-
-# Function to reverse the effects of differencing data
-def reverse_difference(data, order=1, period=1):
-    if order > 2:
-        print('Order of undifferencing higher than 2 not implemented')
-        return data
-    else:
-        # Undifference the data
-        undifferenced_data = pm.utils.diff_inv(data, lag=period)[period:]
-
-        if order == 2:
-            # Undifference again
-            undifferenced_data2 = pm.utils.diff_inv(undifferenced_data, lag=period)[period:]
-            return undifferenced_data2
-        else:
-            return undifferenced_data
-
-# Function to difference data if necessary and build appropriate model
-def predict_data(data, model_type='AR'):
-    # Determine the period of the seasonality (if any)
-    # periods = 1 indicates there is no seasonality
-    periods = predict_seasonality(data)
-
-    # Initially set orders of differencing to 0 (no differencing neccessary)
-    order_of_differencing = 0
-    order_of_seasonal_differencing = 0
-
-    # Check for differencing
-    order_of_differencing = pm.arima.ndiffs(data, test='adf', max_d=2)
-
-    # If there is seasonality
-    if periods > 1:
-        # Check for seasonal differencing
-        order_of_seasonal_differencing = pm.arima.nsdiffs(data, periods)
-
-    # Perform seasonal differencing first, if applicable
-    if order_of_seasonal_differencing > 0:
-        # Perform seasonal differencing
-        seasonally_differenced_data = difference(data, order_of_seasonal_differencing, periods)
-
-        # Check for further regular differencing
-        order_of_differencing = pm.arima.ndiffs(data, test='adf', max_d=2)
-
-        # Difference if applicable, and predict future values
-        if order_of_differencing > 0:
-            differenced_data = difference(seasonally_differenced_data, order_of_differencing, 1)
-            full_data, num_predictions = model_predict(differenced_data, model_type)
-        else:
-            full_data, num_predictions = model_predict(seasonally_differenced_data, model_type)
-
-        # Undifference data and predictions to revert the dataset to its original values
-        seasonally_undifferenced_data = reverse_difference(full_data, order_of_seasonal_differencing, periods)
-
-        # If regular differencing also occurred, perform regular undifferencing
-        if order_of_differencing > 0:
-            full_data_undifferenced = reverse_difference(seasonally_undifferenced_data, order_of_differencing, 1)
-            return full_data_undifferenced, num_predictions
-        else:
-            return seasonally_undifferenced_data, num_predictions
-    elif order_of_differencing > 0:
-        # Only non-seasonal differencing is required
-        differenced_data = difference(data, order_of_differencing, 1)
-
-        full_data, num_predictions = model_predict(differenced_data, model_type)
-
-        # Undifference the dataset with predictions
-        full_data_undifferenced = reverse_difference(full_data, order_of_differencing, 1)
-        return full_data_undifferenced, num_predictions
-    else:
-        # No differencing is required
-        full_data, num_predictions = model_predict(data, model_type)
-        return full_data, num_predictions
 
 # Function to replace the index of a dataset with a suitable pandas Index
 def replace_index(data, index_name='Index'):
@@ -512,6 +303,11 @@ def predict(url, xlabel='x-axis', ylabel='y-axis', title='Title of Line Graph', 
 
     fig, ax = plt.subplots()
 
+    # Set axis labels and title
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    plt.title(title)
+
     # Catch incorrect data type execeptions when trying to plot data
     try:
         if graph_choice == 'Line Graph':
@@ -526,13 +322,10 @@ def predict(url, xlabel='x-axis', ylabel='y-axis', title='Title of Line Graph', 
         elif graph_choice == 'Horizontal Bar Chart':
             ax.barh(full_data[:-num_predictions + 1].index.values, full_data[:-num_predictions + 1])
             ax.barh(full_data[-num_predictions:].index.values, full_data[-num_predictions:], color='red')
+            ax.set_xlabel(ylabel)
+            ax.set_ylabel(xlabel)
     except TypeError as te:
         return str(str(te) + ' has occurred, check column choices')
-
-    # Set axis labels and title
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-    plt.title(title)
 
     # Display plot
     plt.show()
