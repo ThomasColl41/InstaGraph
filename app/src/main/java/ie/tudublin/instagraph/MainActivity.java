9a9e973125fa760001728bf2b09e9ad5f72e6559
Thomas Coll <thomascoll41@gmail.com>
2022-03-20 22:04:38 +0000

    Added Popup.java, a class that displays popup messages. Removed the showInfoPopup method in MainActivity.java and replaced it with a Popup object.

--- app/src/main/java/ie/tudublin/instagraph/MainActivity.java
+++ app/src/main/java/ie/tudublin/instagraph/MainActivity.java
@@ -44,55 +44,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             case(R.id.info_button):
                 // Display info pop-up
-                showInfoPopup();
+                Popup p = new Popup(MainActivity.this, mainLayout);
+                p.showPopup(R.string.instagraph_information);
                 break;
         }
-    }
-
-    public void showInfoPopup() {
-        View infoView;
-
-        // Create inflater
-        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-        try {
-            // Inflate the popup (resource (xml), root, attachToRoot)
-            // attachToRoot is false because only the LayoutParams of mainLayout are required
-            infoView = inflater.inflate(R.layout.popup, mainLayout, false);
-        }
-        catch (InflateException ie) {
-            Log.i("InstaGraph", ie.getMessage());
-            Toast.makeText(
-                    this,
-                    "Error displaying information (" + ie.getMessage() + ")",
-                    Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        // Create PopupWindow (view, width, height, focusable)
-        // focusable is true so the popup can be dismissed by tapping anywhere
-        PopupWindow infoPopup = new PopupWindow(
-                infoView,
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                true);
-
-        // Identify the TexView in the popup xml and set its text dynamically
-        TextView popupText = infoView.findViewById(R.id.popup_textview);
-        popupText.setText(R.string.instagraph_information);
-
-        // Display the popup in the center of the screen (layout)
-        infoPopup.showAtLocation(mainLayout, Gravity.CENTER, 0,0);
-
-        // Set up an onTouchListener to react to the user tapping the screen
-        // A lambda method is used for the onTouch method
-        infoView.setOnTouchListener((view, motionEvent) -> {
-            // Hide the popup
-            infoPopup.dismiss();
-
-            // Perform a click to call the OnClickListener and register the clicking event
-            // for sound preferences and accessibility features
-            view.performClick();
-            return true;
-        });
     }
 }
---
+++ app/src/main/java/ie/tudublin/instagraph/Popup.java
@@ -0,0 +1,100 @@
+package ie.tudublin.instagraph;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.InflateException;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+public class Popup {
+    // Attributes
+    Context context;
+    RelativeLayout parentLayout;
+
+    public Popup(Context context, RelativeLayout parentLayout) {
+        this.context = context;
+        this.parentLayout = parentLayout;
+    }
+
+    public void showPopup(int text) {
+        View popupView;
+
+        // Create inflater
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        try {
+            // Inflate the popup (resource (xml), root, attachToRoot)
+            // attachToRoot is false because only the LayoutParams of mainLayout are required
+            popupView = inflater.inflate(R.layout.popup, parentLayout, false);
+        }
+        catch (InflateException ie) {
+            Log.i("InstaGraph", ie.getMessage());
+            Toast.makeText(
+                    context,
+                    "Error displaying information (" + ie.getMessage() + ")",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Create PopupWindow (view, width, height, focusable)
+        // focusable is true so the popup can be dismissed by tapping anywhere
+        PopupWindow infoPopup = new PopupWindow(
+                popupView,
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                true);
+
+        // Identify the TextView in the popup xml and set its text dynamically
+        TextView popupText = popupView.findViewById(R.id.popup_textview);
+        popupText.setText(text);
+
+        // Display the popup in the center of the screen (layout)
+        infoPopup.showAtLocation(parentLayout, Gravity.CENTER, 0,0);
+
+        // Set up an onTouchListener to react to the user tapping the screen
+        // A lambda method is used for the onTouch method
+        popupView.setOnTouchListener((view, motionEvent) -> {
+            // Hide the popup
+            infoPopup.dismiss();
+
+            // Perform a click to call the OnClickListener and register the clicking event
+            // for sound preferences and accessibility features
+            view.performClick();
+            return true;
+        });
+    }
+
+    // Getters and Setters
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public RelativeLayout getParentLayout() {
+        return parentLayout;
+    }
+
+    public void setParentLayout(RelativeLayout parentLayout) {
+        this.parentLayout = parentLayout;
+    }
+
+
+    //toString
+    @NonNull
+    @Override
+    public String toString() {
+        return "Popup{" +
+                "context=" + context +
+                ", parentLayout=" + parentLayout +
+                '}';
+    }
+}
